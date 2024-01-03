fe0bae5638800cd65630eab610c04b0b64de7345
Andrew D <56039221+andent2@users.noreply.github.com>
2024-01-03 15:44:50 -0500

    Update price label component

---
+++ components/label.tsx
@@ -0,0 +1,34 @@
+import clsx from 'clsx';
+import Price from './price';
+
+const Label = ({
+  title,
+  amount,
+  currencyCode,
+  position = 'bottom'
+}: {
+  title: string;
+  amount: string;
+  currencyCode: string;
+  position?: 'bottom' | 'center';
+}) => {
+  return (
+    <div
+      className={clsx('absolute bottom-0 left-0 flex w-full px-4 pb-4 @container/label', {
+        'lg:px-20 lg:pb-[35%]': position === 'center'
+      })}
+    >
+      <div className="flex items-center rounded-lg border border-gray-800 bg-emerald-300/80 p-1 text-xs font-semibold text-black backdrop-blur-md transition-all ease-in-out group-hover:bg-emerald-400">
+        <h3 className="mr-4 line-clamp-2 flex-grow pl-2 leading-none tracking-tight">{title}</h3>
+        <Price
+          className="flex-none rounded bg-white p-2 text-black"
+          amount={amount}
+          currencyCode={currencyCode}
+          currencyCodeClassName="hidden @[275px]/label:inline"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Label;
