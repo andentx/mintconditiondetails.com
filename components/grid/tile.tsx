e7049794b387355dec6ae5c93d89482f584febd3
Andrew D <56039221+andent2@users.noreply.github.com>
2024-01-03 15:45:10 -0500

    Update tile component

---
+++ components/grid/tile.tsx
@@ -0,0 +1,50 @@
+import clsx from 'clsx';
+import Image from 'next/image';
+import Label from '../label';
+
+export function GridTileImage({
+  isInteractive = true,
+  active,
+  label,
+  ...props
+}: {
+  isInteractive?: boolean;
+  active?: boolean;
+  label?: {
+    title: string;
+    amount: string;
+    currencyCode: string;
+    position?: 'bottom' | 'center';
+  };
+} & React.ComponentProps<typeof Image>) {
+  return (
+    <div
+      className={clsx(
+        'group flex h-full w-full items-center justify-center overflow-hidden rounded-lg border border-gray-800 bg-emerald-300',
+        {
+          relative: label,
+          'border-2 border-emerald-300': active,
+          'border-neutral-200': !active
+        }
+      )}
+    >
+      {props.src ? (
+        // eslint-disable-next-line jsx-a11y/alt-text -- `alt` is inherited from `props`, which is being enforced with TypeScript
+        <Image
+          className={clsx('relative h-full w-full object-contain', {
+            'transition-all ease-in-out group-hover:opacity-60': isInteractive
+          })}
+          {...props}
+        />
+      ) : null}
+      {label ? (
+        <Label
+          title={label.title}
+          amount={label.amount}
+          currencyCode={label.currencyCode}
+          position={label.position}
+        />
+      ) : null}
+    </div>
+  );
+}
