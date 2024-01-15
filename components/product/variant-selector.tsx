c7c1417b7ee948cbc06c227297c4a7c31e9a2e71
Andrew D <56039221+andent2@users.noreply.github.com>
2024-01-15 18:39:19 -0500

    Update variant selector styles

--- components/product/variant-selector.tsx
+++ components/product/variant-selector.tsx
@@ -86,10 +86,10 @@ export function VariantSelector({
               }}
               title={`${option.name} ${value}${!isAvailableForSale ? ' (Out of Stock)' : ''}`}
               className={clsx(
-                'flex min-w-[48px] items-center justify-center rounded-full border bg-neutral-100 px-2 py-1 text-sm',
+                'flex min-w-[48px] items-center justify-center rounded-full border bg-neutral-100 px-4 py-2 text-sm',
                 {
-                  'cursor-default ring-2 ring-blue-600': isActive,
-                  'ring-1 ring-transparent transition duration-300 ease-in-out hover:scale-110 hover:ring-blue-600 ':
+                  'cursor-default ring-2 ring-emerald-400': isActive,
+                  'ring-1 ring-transparent transition duration-300 ease-in-out hover:scale-110 hover:ring-emerald-400 ':
                     !isActive && isAvailableForSale,
                   'relative z-10 cursor-not-allowed overflow-hidden bg-neutral-100 text-neutral-500 ring-1 ring-neutral-300 before:absolute before:inset-x-0 before:-z-10 before:h-px before:-rotate-45 before:bg-neutral-300 before:transition-transform':
                     !isAvailableForSale
