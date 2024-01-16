da410cfc635353f6d7ca5ac799592aed60d56982
Andrew D <56039221+andent2@users.noreply.github.com>
2024-01-16 16:16:54 -0500

    Add contact section component to contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,3 +1,5 @@
+import ContactSection from 'components/contact-section';
+
 export const runtime = 'edge';
 
 export const metadata = {
@@ -10,9 +12,7 @@ export default async function ContactPage() {
     <>
       <div className="mx-auto w-full max-w-screen-2xl px-4 ">
         <h4 className="mb-6 w-full text-4xl font-bold tracking-tight text-gray-800">Contact</h4>
-        <div className="mx-auto mb-20 flex h-[30rem] w-full max-w-screen-2xl items-center justify-center rounded-lg bg-emerald-300">
-          <p className="text-center">contact section</p>
-        </div>
+        <ContactSection />
       </div>
     </>
   );
