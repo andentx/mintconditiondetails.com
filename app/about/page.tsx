a7d4afccafde885487138b873858ef06c646e1d7
Andrew D <56039221+andent2@users.noreply.github.com>
2024-01-16 14:51:15 -0500

    Add content to about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+
 export const runtime = 'edge';
 
 export const metadata = {
@@ -9,9 +11,30 @@ export default async function AboutPage() {
   return (
     <>
       <div className="mx-auto w-full max-w-screen-2xl px-4 ">
-        <h4 className="mb-6 w-full text-4xl font-bold tracking-tight text-gray-800">About</h4>
-        <div className="mx-auto mb-20 flex h-[30rem] w-full max-w-screen-2xl items-center justify-center rounded-lg bg-emerald-300">
-          <p className="text-center">about section</p>
+        <h2 className="mb-6 w-full text-4xl font-bold tracking-tight text-gray-800">About</h2>
+        <div className="mx-auto mb-20 flex h-[30rem] w-full flex-col  md:flex-row">
+          <div className="text-md flex flex-col pb-10 md:mb-0 md:w-1/2 md:pr-10">
+            <p className="mb-6 ">
+              <span className="font-bold">Hi, I&apos;m Andrew. </span> I&apos;m a detailer
+              specializing in paint correction and ceramic coatings. I also make web-based projects,
+              and this is one of them.
+            </p>
+            <p className="mb-6">
+              After a decade of detailing experience, I&apos;ve developed preferences and processes
+              that help me achieve extraordinary results. This website is a place for me to offer
+              services as well as share my knowledge in hopes of helping you achieve exceptional
+              results as well.
+            </p>
+          </div>
+          <div className="flex flex-col md:w-1/2">
+            <Image
+              src="https://res.cloudinary.com/dsz45zrla/image/upload/v1705431100/detailer_hr9nsb.png"
+              alt="A guitar on a workbench"
+              className="w-full max-w-screen-2xl rounded-md border border-gray-800 object-contain"
+              height="1200"
+              width="1600"
+            />
+          </div>
         </div>
       </div>
     </>
