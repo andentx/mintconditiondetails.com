9566f301e68a6818e6151bf7f0d6cc894d3db338
Andrew D <56039221+andent2@users.noreply.github.com>
2024-01-17 18:23:22 -0500

    Update services components

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -26,19 +26,13 @@ export default async function ServicesPage() {
     <>
       <div className="mx-auto w-full max-w-screen-2xl px-4 ">
         <h4 className="mb-6 w-full text-4xl font-bold tracking-tight text-gray-800">Services</h4>
-        <div className="mx-auto mb-20 flex h-[30rem] w-full max-w-screen-2xl items-center justify-center rounded-lg bg-emerald-300">
-          <p className="text-center">services section</p>
-        </div>
-      </div>
-      <h4 className="mx-auto mb-6 max-w-screen-2xl px-4 text-4xl font-bold tracking-tight text-gray-800">
-        Our Services
-      </h4>
 
-      <section className="mx-auto mb-10 grid max-w-screen-2xl grid-cols-1 gap-4 px-4 pb-4 md:grid-cols-2 lg:grid-cols-3">
-        {servicesMenuSection.map((service, index) => {
-          return <MenuSection key={index} title={service.title} subtitle={service.subtitle} />;
-        })}
-      </section>
+        <section className="mx-auto mb-10 grid max-w-screen-2xl grid-cols-1 gap-4 px-4 pb-4 md:grid-cols-2 lg:grid-cols-3">
+          {servicesMenuSection.map((service, index) => {
+            return <MenuSection key={index} title={service.title} subtitle={service.subtitle} />;
+          })}
+        </section>
+      </div>
     </>
   );
 }
