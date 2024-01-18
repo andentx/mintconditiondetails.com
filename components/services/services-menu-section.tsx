e39954dabc7f276f7e8bbeedd65ad2a345f67879
Andrew D <56039221+andent2@users.noreply.github.com>
2024-01-18 15:52:58 -0500

    Fix errors in service menu section

--- components/services/services-menu-section.tsx
+++ components/services/services-menu-section.tsx
@@ -1,3 +1,6 @@
+// @ts-ignore
+import BlockContent from '@sanity/block-content-to-react';
+
 type MenuSectionProps = {
   title: string;
   subtitle: string;
@@ -6,15 +9,12 @@ type MenuSectionProps = {
 
 type MenuItem = {
   title: string;
-  id: string;
+  index: string;
   description: { children: Array<{ text: string }> }[];
-  descriptionBulleted: { children: Array<{ text: string }> }[];
   price: string;
 };
 
 export function MenuSection({ title, subtitle, menuItems }: MenuSectionProps) {
-  const sectionTitle = title;
-  const sectionSubtitle = subtitle;
   const items = menuItems;
 
   console.log(items);
@@ -23,27 +23,27 @@ export function MenuSection({ title, subtitle, menuItems }: MenuSectionProps) {
       <div className="bg-blue-200">
         <div className="mx-auto grid max-w-2xl grid-cols-1 items-start gap-x-8 gap-y-16 px-4 py-16 sm:px-6 sm:py-16 lg:max-w-7xl lg:grid-cols-2 lg:px-8">
           <div>
-            <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-              {sectionTitle}
-            </h2>
-            <p className="mt-4 text-gray-500">{sectionSubtitle}</p>
+            <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">{title}</h2>
+            <p className="mt-4 text-gray-500">{subtitle}</p>
+            <div className="mt-4 bg-purple-200 text-gray-500">
+              {menuItems.map((item) => (
+                <div key={item.index} className="bg-green-200">
+                  <h3>{item.title}</h3>
 
-            {/* <dl className="grid grid-cols-1 mt-16 gap-x-6 gap-y-10 sm:grid-cols-2 sm:gap-y-16 lg:gap-x-8">
-              {items.map((feature) => {
-                return (
-                  <div
-                    key={feature.id}
-                    className="flex flex-col pt-4 border-t-2 border-gray-200 border-solid "
-                  >
-                    <dt className="font-medium text-gray-900">{feature.title}</dt>
+                  {item.description ? (
+                    <BlockContent
+                      blocks={item.description}
+                      projectId={process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}
+                      dataset={process.env.NEXT_PUBLIC_SANITY_DATASET}
+                    />
+                  ) : (
+                    <p>No detailed description available.</p>
+                  )}
 
-                    <dd className="mt-2 text-sm text-gray-900 font-montserratBold">
-                      {feature.price}
-                    </dd>
-                  </div>
-                );
-              })}
-            </dl> */}
+                  <p className="font-montserratBold mt-2 text-sm text-gray-900">{item.price}</p>
+                </div>
+              ))}
+            </div>
           </div>
         </div>
       </div>
