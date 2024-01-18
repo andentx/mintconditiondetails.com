1c0357438bb69c1d5278bb655102764dbe49c860
Andrew D <56039221+andent2@users.noreply.github.com>
2024-01-18 17:58:28 -0500

    Refactor block content to html

--- components/services/services-menu-section.tsx
+++ components/services/services-menu-section.tsx
@@ -1,6 +1,3 @@
-// @ts-ignore
-import BlockContent from '@sanity/block-content-to-react';
-
 type MenuSectionProps = {
   title: string;
   subtitle: string;
@@ -15,9 +12,6 @@ type MenuItem = {
 };
 
 export function MenuSection({ title, subtitle, menuItems }: MenuSectionProps) {
-  const items = menuItems;
-
-  console.log(items);
   return (
     <>
       <div className="bg-blue-200">
@@ -26,23 +20,19 @@ export function MenuSection({ title, subtitle, menuItems }: MenuSectionProps) {
             <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">{title}</h2>
             <p className="mt-4 text-gray-500">{subtitle}</p>
             <div className="mt-4 bg-purple-200 text-gray-500">
-              {menuItems.map((item) => (
-                <div key={item.index} className="bg-green-200">
-                  <h3>{item.title}</h3>
-
-                  {item.description ? (
-                    <BlockContent
-                      blocks={item.description}
-                      projectId={process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}
-                      dataset={process.env.NEXT_PUBLIC_SANITY_DATASET}
-                    />
-                  ) : (
-                    <p>No detailed description available.</p>
-                  )}
-
-                  <p className="font-montserratBold mt-2 text-sm text-gray-900">{item.price}</p>
-                </div>
-              ))}
+              {menuItems.map((item) => {
+                return (
+                  <div key={item.index} className="bg-green-200">
+                    <h3>{item.title}</h3>
+                    <dd className="mb-2 mt-2 text-sm text-gray-500 ">
+                      {item.description.map((descriptionElement, index) => (
+                        <p key={index}>{descriptionElement.children[0]?.text}</p>
+                      ))}
+                    </dd>
+                    <p className="font-montserratBold mt-2 text-sm text-gray-900">{item.price}</p>
+                  </div>
+                );
+              })}
             </div>
           </div>
         </div>
