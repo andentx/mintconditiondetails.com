bfe428911219c76be69ab37e7af246806f381fb5
Andrew D <56039221+andent2@users.noreply.github.com>
2024-01-19 15:40:14 -0500

    Update services menu styles

--- components/services/services-menu-section.tsx
+++ components/services/services-menu-section.tsx
@@ -27,26 +27,36 @@ export function MenuSection({ title, subtitle, menuItems, images }: MenuSectionP
           <div>
             <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">{title}</h2>
             <p className="mt-4 text-gray-500">{subtitle}</p>
-            <div className="mt-4 bg-purple-200 text-gray-500">
+
+            <dl className="mt-16 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 sm:gap-y-16 lg:gap-x-8">
               {menuItems.map((item) => {
-                // console.log(item);
                 return (
-                  <div key={item.index} className="bg-green-200">
-                    <h3>{item.title}</h3>
+                  <div
+                    key={item.index}
+                    className="flex flex-col border-t-2 border-solid border-gray-200 pt-4 "
+                  >
+                    <dt className="font-medium text-gray-900">{item.title}</dt>
                     <dd className="mb-2 mt-2 text-sm text-gray-500 ">
-                      {item.description.map((descriptionElement, index) => (
-                        <p key={index}>{descriptionElement.children[0]?.text}</p>
-                      ))}
+                      <div>
+                        {item.description.map((descriptionElement, index) => {
+                          return (
+                            <p key={index} className="">
+                              {descriptionElement.children[0]?.text}
+                            </p>
+                          );
+                        })}
+                      </div>
                     </dd>
-                    <p className="font-montserratBold mt-2 text-sm text-gray-900">{item.price}</p>
+                    <dd className="font-montserratBold mt-2 text-sm text-gray-900">{item.price}</dd>
                   </div>
                 );
               })}
-            </div>
+            </dl>
           </div>
+
           <div className="grid grid-cols-2 grid-rows-2 gap-4 sm:mt-4 sm:gap-6 lg:mt-14 lg:gap-8">
             {images.map((image, idx) => (
-              <div key={idx} className="h-64 w-64 bg-red-200" />
+              <div key={idx} className="h-64 w-64 rounded-lg bg-red-200" />
             ))}
           </div>
         </div>
