09611eab640fad106ed9962b59dd680e3d119fad
Andrew D <56039221+andent2@users.noreply.github.com>
2024-01-23 17:14:25 -0500

    Refactor layout

--- app/[page]/layout.tsx
+++ app/[page]/layout.tsx
@@ -3,10 +3,8 @@ import { Suspense } from 'react';
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <Suspense>
-      <div className="w-full">
-        <div className="mx-8 max-w-2xl py-20 sm:mx-auto">
-          <Suspense>{children}</Suspense>
-        </div>
+      <div className="w-full px-4">
+        <Suspense>{children}</Suspense>
       </div>
     </Suspense>
   );
