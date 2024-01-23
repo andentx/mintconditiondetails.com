27924a7c58c9947b98fd78d525dad05852aeeb10
Andrew D <56039221+andent2@users.noreply.github.com>
2024-01-23 12:20:33 -0500

    Change rounded corners to md

--- components/landing-hero.tsx
+++ components/landing-hero.tsx
@@ -7,19 +7,19 @@ export default function LandingHero() {
         <Image
           src="https://res.cloudinary.com/dsz45zrla/image/upload/v1703008119/Image_1_dtzv0k.jpg"
           alt="A guitar on a workbench"
-          className="h-full w-full max-w-screen-2xl rounded-lg border border-gray-800 object-cover object-center"
+          className="h-full w-full max-w-screen-2xl rounded-md border border-gray-800 object-cover object-center"
           height="1200"
           width="1600"
         />
       </div>
 
       <div className="relative mx-auto flex max-w-5xl flex-col items-center px-6 pb-44 pt-24 text-center sm:pb-60 lg:pb-96 1280px:pb-[32rem]">
-        <h1 className="mb-4 w-full max-w-xs rounded-lg border border-gray-800 bg-white/80 py-4 text-xl backdrop-blur-sm sm:max-w-lg sm:text-4xl md:max-w-2xl md:text-5xl lg:mb-12 lg:max-w-3xl lg:text-6xl">
+        <h1 className="mb-4 w-full max-w-xs rounded-md border border-gray-800 bg-white/80 py-4 text-xl backdrop-blur-sm sm:max-w-lg sm:text-4xl md:max-w-2xl md:text-5xl lg:mb-12 lg:max-w-3xl lg:text-6xl">
           <span className="font-bold text-gray-800 ">Mint</span>
           <span className="mr-[0.5ch] font-light text-gray-800">Condition</span>
           <span className="font-bold text-gray-800">Detailing</span>
         </h1>
-        <p className="mb-12 w-full max-w-xs rounded-lg border border-gray-800 bg-white/80 px-6 py-4 text-base text-black backdrop-blur-sm sm:max-w-lg sm:text-lg lg:text-xl ">
+        <p className="mb-12 w-full max-w-xs rounded-md border border-gray-800 bg-white/80 px-6 py-4 text-base text-black backdrop-blur-sm sm:max-w-lg sm:text-lg lg:text-xl ">
           A boutique detailing shop specializing in paint correction and ceramic coatings.
         </p>
 
