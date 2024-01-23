aff57b130a8cb5956fad952db32e9544a1c42249
Andrew D <56039221+andent2@users.noreply.github.com>
2024-01-23 16:35:42 -0500

    Add full width to shop page

--- app/shop/layout.tsx
+++ app/shop/layout.tsx
@@ -6,11 +6,11 @@ import { Suspense } from 'react';
 export default function SearchLayout({ children }: { children: React.ReactNode }) {
   return (
     <Suspense>
-      <div className="mx-auto flex max-w-screen-2xl flex-col gap-8 px-4 pb-4 text-black md:flex-row">
+      <div className="mx-auto flex w-full max-w-screen-2xl flex-col gap-8 px-4 pb-4 text-black md:flex-row">
         <div className="order-first w-full flex-none md:max-w-[125px]">
           <Collections />
         </div>
-        <div className="order-last w-full bg-blue-300 md:order-none">{children}</div>
+        <div className="order-last w-full md:order-none">{children}</div>
         <div className="order-none flex-none md:order-last md:w-[125px]">
           <FilterList list={sorting} title="Sort by" />
         </div>
