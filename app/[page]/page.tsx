f84662476cddfe1261bff7798b7e315cdd21a9aa
Andrew D <56039221+andent2@users.noreply.github.com>
2024-01-23 17:30:40 -0500

    Update dynamic shopify page styling

--- app/[page]/page.tsx
+++ app/[page]/page.tsx
@@ -35,15 +35,20 @@ export default async function Page({ params }: { params: { page: string } }) {
 
   return (
     <>
-      <h1 className="mb-8 text-5xl font-bold">{page.title}</h1>
-      <Prose className="mb-8" html={page.body as string} />
-      <p className="text-sm italic">
-        {`This document was last updated on ${new Intl.DateTimeFormat(undefined, {
-          year: 'numeric',
-          month: 'long',
-          day: 'numeric'
-        }).format(new Date(page.updatedAt))}.`}
-      </p>
+      <div className="flex w-full flex-col items-center ">
+        <h4 className="mb-6 w-full text-4xl font-bold tracking-tight text-gray-800">
+          {page.title}
+        </h4>
+
+        <Prose className="mb-40" html={page.body as string} />
+        <p className="w-full text-center text-sm italic ">
+          {`This document was last updated on ${new Intl.DateTimeFormat(undefined, {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric'
+          }).format(new Date(page.updatedAt))}.`}
+        </p>
+      </div>
     </>
   );
 }
