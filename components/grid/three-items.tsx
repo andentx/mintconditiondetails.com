5498f7be8c6d2879e650bb822e3d599e7b0ead81
Andrew D <56039221+andent2@users.noreply.github.com>
2024-01-23 14:46:18 -0500

    Add full width to three item grid

--- components/grid/three-items.tsx
+++ components/grid/three-items.tsx
@@ -52,7 +52,7 @@ export async function ThreeItemGrid() {
 
   return (
     <>
-      <section className="mx-auto grid max-w-screen-2xl gap-4 px-4 pb-4 md:grid-cols-6 md:grid-rows-2">
+      <section className="mx-auto mb-10 grid w-full max-w-screen-2xl gap-4 px-4 pb-4 md:grid-cols-6 md:grid-rows-2">
         <ThreeItemGridItem size="full" item={firstProduct} priority={true} />
         <ThreeItemGridItem size="half" item={secondProduct} priority={true} />
         <ThreeItemGridItem size="half" item={thirdProduct} />
