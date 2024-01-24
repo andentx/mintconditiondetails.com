ee952509827d8d4099706be4acfcc42f2bac1b8f
Andrew D <56039221+andent2@users.noreply.github.com>
2024-01-24 17:43:58 -0500

    Refactor services button to use global mintButton css

--- components/grid/landing-services.tsx
+++ components/grid/landing-services.tsx
@@ -9,10 +9,7 @@ export async function LandingServicesGrid() {
     <>
       <section className="mx-auto mb-20 grid max-w-screen-2xl grid-cols-1 gap-4 px-4 pb-4 md:grid-cols-2 lg:grid-cols-3">
         {services.map((service, index) => (
-          <div
-            key={index}
-            className={`flex h-40 w-full items-center justify-center rounded-lg bg-emerald-300 md:h-60 lg:h-96`}
-          >
+          <div key={index} className={`mintButton h-40 w-full md:h-60 lg:h-96`}>
             <p>{service.text}</p>
           </div>
         ))}
