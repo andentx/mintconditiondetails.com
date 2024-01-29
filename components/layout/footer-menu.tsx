26cbb4ebd996ab8436fd29810edec4dd24aa296d
Andrew D <56039221+andent2@users.noreply.github.com>
2024-01-29 14:14:59 -0500

    Add rounded corners to footer links

--- components/layout/footer-menu.tsx
+++ components/layout/footer-menu.tsx
@@ -19,7 +19,7 @@ const FooterMenuItem = ({ item }: { item: Menu }) => {
       <Link
         href={item.path}
         className={clsx(
-          'block p-2 text-lg underline-offset-4 hover:text-black hover:underline md:inline-block md:text-sm',
+          '  block rounded p-2 text-lg underline-offset-4 hover:text-black hover:underline md:inline-block md:text-sm',
           {
             'text-black': active
           }
