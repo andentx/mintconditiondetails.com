24268e7e589a7ad987c27ec5cd181301cc91841d
Andrew D <56039221+andent2@users.noreply.github.com>
2024-01-29 08:24:06 -0500

    Update tile image styles

--- components/grid/tile.tsx
+++ components/grid/tile.tsx
@@ -16,11 +16,11 @@ export function GridTileImage({
   };
 } & React.ComponentProps<typeof Image>) {
   return (
-    <div className="group flex h-full w-full items-center justify-center overflow-hidden rounded-lg border border-neutral-500 bg-neutral-500">
+    <div className="group flex h-full w-full items-center justify-center overflow-hidden border border-neutral-500 bg-neutral-500 hover:cursor-pointer">
       {props.src ? (
         // eslint-disable-next-line jsx-a11y/alt-text -- `alt` is inherited from `props`, which is being enforced with TypeScript
         <Image
-          className="relative h-full w-full object-cover transition-all ease-in-out group-hover:opacity-60"
+          className="relative h-full w-full object-cover transition-all ease-in-out group-hover:opacity-60 group-focus:opacity-60 "
           {...props}
         />
       ) : null}
