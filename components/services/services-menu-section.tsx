ea6e4a6d1b8f6c2385a14abc30304d727c8d4f10
Andrew D <56039221+andent2@users.noreply.github.com>
2024-01-30 16:13:31 -0500

    Update menu item styles

--- components/services/services-menu-section.tsx
+++ components/services/services-menu-section.tsx
@@ -9,22 +9,22 @@ export function MenuSection({ title, subtitle, menuItems, images }: MenuSectionP
       <div className="mx-auto grid w-full max-w-2xl grid-cols-1 items-start gap-x-8 gap-y-16 py-16 sm:py-16 lg:max-w-full lg:grid-cols-2 ">
         <div>
           <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">{title}</h2>
-          <p className="mt-4 text-gray-500">{subtitle}</p>
+          {subtitle && <p className="mt-4 text-gray-500">{subtitle}</p>}
 
-          <dl className="mt-16 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 sm:gap-y-16 lg:gap-x-8">
+          <dl className="grid grid-cols-1 gap-x-6 gap-y-10 pt-4 sm:grid-cols-2 sm:gap-y-16 lg:gap-x-8">
             {menuItems.map((item) => {
               return (
                 <div
                   key={item._id}
                   className="flex flex-col border-t-2 border-solid border-gray-200 pt-4 "
                 >
-                  <dt className="font-medium text-gray-900">{item.title}</dt>
+                  <dt className="text-2xl font-medium text-gray-900">{item.title}</dt>
                   <dd className="mb-2 mt-2 text-sm text-gray-500 ">
                     <span className="portableText">
                       <PortableText value={item.description} />
                     </span>
                   </dd>
-                  <dd className="mt-2 text-sm text-gray-900 ">{item.price}</dd>
+                  <dd className="text mt-2 text-lg font-medium text-gray-900 ">{item.price}</dd>
                 </div>
               );
             })}
