bca3a73a7762e1e84bec4772ce5ac188ae9df6e5
Andrew D <56039221+andent2@users.noreply.github.com>
2024-02-09 09:57:20 -0500

    Revert opengraph image component

--- components/opengraph-image.tsx
+++ components/opengraph-image.tsx
@@ -15,39 +15,26 @@ export default async function OpengraphImage(props?: Props): Promise<ImageRespon
 
   return new ImageResponse(
     (
-      <div tw="flex h-full w-full items-center justify-center bg-white">
-        <div tw="flex items-center justify-center">
-          <LogoIcon width="30" height="68" fill="#34d399" />
-          <div tw="ml-6 text-6xl flex">
-            <p tw="hidden">{title}</p>
-            <span tw="font-bold text-gray-800 ">Mint</span>
-            <span tw="mr-4 font-light text-gray-800">Condition</span>
-            <span tw="font-bold text-gray-800">Detailing</span>
-          </div>
+      <div tw="flex h-full w-full flex-col items-center justify-center bg-black">
+        <div tw="flex flex-none items-center justify-center border border-neutral-700 h-[160px] w-[160px] rounded-3xl">
+          <LogoIcon width="64" height="58" fill="white" />
         </div>
+        <p tw="mt-12 text-6xl font-bold text-white">{title}</p>
       </div>
     ),
     {
       width: 1200,
-      height: 630,
-      fonts: [
-        {
-          name: 'Inter',
-          data: await fetch(new URL('../fonts/Inter-Bold.ttf', import.meta.url)).then((res) =>
-            res.arrayBuffer()
-          ),
-          style: 'normal',
-          weight: 700
-        },
-        {
-          name: 'Inter',
-          data: await fetch(new URL('../fonts/Inter-Light.ttf', import.meta.url)).then((res) =>
-            res.arrayBuffer()
-          ),
-          style: 'normal',
-          weight: 300
-        }
-      ]
+      height: 630
+      // fonts: [
+      //   {
+      //     name: 'Inter',
+      //     data: await fetch(new URL('../fonts/Inter-Bold.ttf', import.meta.url)).then((res) =>
+      //       res.arrayBuffer()
+      //     ),
+      //     style: 'normal',
+      //     weight: 700
+      //   }
+      // ]
     }
   );
 }
