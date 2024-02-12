561211ab0231fefb9d07dac7222ad7cc83f8a52a
Andrew D <56039221+andent2@users.noreply.github.com>
2024-02-12 15:13:08 -0500

    Add label to search form

--- components/layout/navbar/search.tsx
+++ components/layout/navbar/search.tsx
@@ -26,6 +26,9 @@ export default function Search() {
 
   return (
     <form onSubmit={onSubmit} className="w-max-[550px] relative w-80">
+      <label htmlFor="search" className="sr-only">
+        Search for products
+      </label>
       <input
         key={searchParams?.get('q')}
         type="text"
