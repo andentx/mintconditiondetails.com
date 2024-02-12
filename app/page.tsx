57b62197eee63dd5f6baf748223236552bae5730
Andrew D <56039221+andent2@users.noreply.github.com>
2024-02-12 13:30:09 -0500

    Update servicesMenuSection sorting method

--- app/page.tsx
+++ app/page.tsx
@@ -32,7 +32,7 @@ export const dynamic = 'force-dynamic';
 
 export default async function HomePage() {
   const homePageServices = await client.fetch<HomepageFeaturedService[]>(
-    `*[_type == "homepageFeaturedServices"]`
+    `*[_type == "homepageFeaturedServices"] | order(index asc)`
   );
 
   return (
@@ -42,37 +42,35 @@ export default async function HomePage() {
         Our Services
       </h4>
       <section className="mx-auto mb-10 grid w-full max-w-screen-2xl grid-cols-1 gap-4 px-4 pb-4 md:grid-cols-2 lg:grid-cols-3">
-        {homePageServices
-          .sort((a, b) => a.index - b.index) // Sort the array by index
-          .map((service, index) => {
-            const imageUrl = service.image.asset._ref
-              ? urlForImage(service.image.asset._ref).height(1000).width(2000).url()
-              : null;
+        {homePageServices.map((service, index) => {
+          const imageUrl = service.image.asset._ref
+            ? urlForImage(service.image.asset._ref).height(1000).width(2000).url()
+            : null;
 
-            return (
-              <Link
-                href="/services"
-                key={index}
-                className="group relative grid h-40 w-full grid-cols-1 items-center justify-items-center overflow-hidden border border-neutral-500 bg-neutral-500 hover:cursor-pointer md:h-60 lg:h-96"
-              >
-                {imageUrl ? (
-                  <Image
-                    className="col-start-1 row-start-1 h-full w-full object-cover transition-all ease-in-out group-hover:opacity-60 group-focus:opacity-60"
-                    fill={true}
-                    alt={service.imageAltText}
-                    src={imageUrl}
-                  />
-                ) : (
-                  <div className="h-full w-full bg-neutral-500 " />
-                )}
-                <div className="z-50 col-start-1 row-start-1 flex h-full w-full flex-col items-center justify-center text-center">
-                  <div className=" mintButton group-hover:bg-emerald-400/80 group-focus:bg-emerald-400/80 group-active:bg-emerald-500">
-                    {service.title}
-                  </div>
+          return (
+            <Link
+              href="/services"
+              key={index}
+              className="group relative grid h-40 w-full grid-cols-1 items-center justify-items-center overflow-hidden border border-neutral-500 bg-neutral-500 hover:cursor-pointer md:h-60 lg:h-96"
+            >
+              {imageUrl ? (
+                <Image
+                  className="col-start-1 row-start-1 h-full w-full object-cover transition-all ease-in-out group-hover:opacity-60 group-focus:opacity-60"
+                  fill={true}
+                  alt={service.imageAltText}
+                  src={imageUrl}
+                />
+              ) : (
+                <div className="h-full w-full bg-neutral-500 " />
+              )}
+              <div className="z-50 col-start-1 row-start-1 flex h-full w-full flex-col items-center justify-center text-center">
+                <div className=" mintButton group-hover:bg-emerald-400/80 group-focus:bg-emerald-400/80 group-active:bg-emerald-500">
+                  {service.title}
                 </div>
-              </Link>
-            );
-          })}
+              </div>
+            </Link>
+          );
+        })}
       </section>
 
       <div className="mx-auto mb-20 flex w-full max-w-screen-2xl justify-center px-4 py-0">
